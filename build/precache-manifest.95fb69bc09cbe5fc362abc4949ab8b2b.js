0cd3cb55dba59970c98f0861a29fea07f04c662e
Lucas Ribeiro <lucasrb04@gmail.com>
2021-09-09 00:40:14 -0300

    testando build

---
+++ build/precache-manifest.95fb69bc09cbe5fc362abc4949ab8b2b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96116131a0bf4628b74c114b02024cdb",
+    "url": "/App-de-receitas-React/index.html"
+  },
+  {
+    "revision": "efc8c6806ef0e4938462",
+    "url": "/App-de-receitas-React/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "87c738ac47bc6742bb65",
+    "url": "/App-de-receitas-React/static/css/main.2faad354.chunk.css"
+  },
+  {
+    "revision": "efc8c6806ef0e4938462",
+    "url": "/App-de-receitas-React/static/js/2.e114af3b.chunk.js"
+  },
+  {
+    "revision": "9353b4df7938eafd5f8e3a0cf5a68c12",
+    "url": "/App-de-receitas-React/static/js/2.e114af3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87c738ac47bc6742bb65",
+    "url": "/App-de-receitas-React/static/js/main.2b262ba1.chunk.js"
+  },
+  {
+    "revision": "18065446c2ff4966df8d",
+    "url": "/App-de-receitas-React/static/js/runtime-main.9682bc6c.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/App-de-receitas-React/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/App-de-receitas-React/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "194f75f8cf27be78d84e3d9e6d19ee1b",
+    "url": "/App-de-receitas-React/static/media/explore.194f75f8.png"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/App-de-receitas-React/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "0b4d4c30f26f9b9e5eb465181b20c519",
+    "url": "/App-de-receitas-React/static/media/logo.0b4d4c30.png"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/App-de-receitas-React/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/App-de-receitas-React/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/App-de-receitas-React/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/App-de-receitas-React/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/App-de-receitas-React/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
